77237946005b694eadcb60d08cfaa4e0bbed0563
Polychronis Stamoulis <59158728+poler2860@users.noreply.github.com>
2023-11-18 15:50:13 +0200

    Update lpButterworth.m
    lpButterworth now returns the numerator and denominator of the digital filter

--- Assignment_3/lpButterworth.m
+++ Assignment_3/lpButterworth.m
@@ -1,8 +1,8 @@
-function lpButterworth(Attenuation, smpl_freq, samples)
+function [num_d, denom_d] = lpButterworth(Attenuation, smpl_freq, samples)
 fs = smpl_freq;             %Sampling frequency
-fn = fs/2;              %Nyquist frequency
-Rp = 3;                 %Ripple  
-Rs = Attenuation;       %Attenuation
+fn = fs/2;                  %Nyquist frequency
+Rp = 3;                     %Ripple  
+Rs = Attenuation;           %Attenuation
 
 %Normalised frequencies
 Wp = 2*pi*3000;
@@ -15,16 +15,16 @@ Ws = 2*pi*4000;
 [z, p, k] = buttap(n);
 
 %Transfare function from zero pole gain
-[num_1, denum_1] = zp2tf(z, p, k);
+[num_1, denom_1] = zp2tf(z, p, k);
 
 %Create filter with cutoff frequency Wn
-[num_2, denum_2] = lp2lp(num_1, denum_1, Wn);
+[num_2, denom_2] = lp2lp(num_1, denom_1, Wn);
 
 %Convert filter to digital
-[num_d, denum_d] = bilinear(num_2, denum_2,fs);
+[num_d, denom_d] = bilinear(num_2, denom_2,fs);
 
-[H_a,~] = freqs(num_2, denum_2, samples);          %Analog filter
-[H_d,~] = freqz(num_d, denum_d, samples, fs);      %Digital filter
+[H_a,~] = freqs(num_2, denom_2, samples);          %Analog filter
+[H_d,~] = freqz(num_d, denom_d, samples, fs);      %Digital filter
 
 f = linspace(0,fn,samples);
 
