53e0ef2d0380d89fff678b9bfc159984dfff647e
crockent <echatzakis1@isc.tuc.gr>
2023-12-05 22:43:07 +0200

    update ex2

--- Assignment_4/Ex2.m
+++ Assignment_4/Ex2.m
@@ -6,7 +6,7 @@ N = [21 , 41];
 freq = 0:0.01:0.5;
 
 % Filter design with hamming window
-h_hamming1 = fir1(N(1)-1, Vc/(Fs/2), 'low', hamming(N(1)));
+h_hamming1 = fir1(N(1)-1, Vc/(Fs/2),'low',hamming(N(1)));
 h_hamming2 = fir1(N(2)-1, Vc/(Fs/2), 'low', hamming(N(2)));
 
 [H_hamming1,hamw1]=freqz(h_hamming1,N(1));
@@ -36,38 +36,43 @@ passSingalFromFiltersWithHz(50,H_hamming1,H_hamming2,H_hanning1,H_hanning2);
 function drawFigureForFirstBullet(figureTitle,yLeftPlot,xLeftPlot,yRightPlot,xRightPlot)
     figure;sgtitle(figureTitle);
     subplot(1,2,1);
-    plot(linspace(0,1,length(xLeftPlot)), 20*log10(abs(yLeftPlot)),'LineWidth',2);
-    grid on;title('Filters Frequency Response');xlabel('Normalize Frequency');ylabel('Magnitude (dB)');
+    plot(linspace(0,1,length(xLeftPlot)), abs(yLeftPlot),'LineWidth',2);
+    grid on;title('Filters Frequency Response');xlabel('Normalize Frequency');ylabel('Magnitude');
     subplot(1,2,2);
-    plot(linspace(0,1,length(xRightPlot)),20*log10(abs(yRightPlot)),'LineWidth',2);
-    grid on;title('Filters Frequency Response');xlabel('Normalize Frequency');ylabel('Magnitude (dB)');
+    plot(linspace(0,1,length(xRightPlot)),abs(yRightPlot),'LineWidth',2);
+    grid on;title('Filters Frequency Response');xlabel('Normalize Frequency');ylabel('Magnitude');
 end
 %function for the second and third bullet
 function passSingalFromFiltersWithHz(Fs,H_hamming1,H_hamming2,H_hanning1,H_hanning2)
-    t = 0:1/Fs:1-1/Fs;
-    x = sin(15*t) + 0.25*sin(200*t);
-    NFFT = 2^nextpow2(length(x));
-    F= -Fs/2:Fs/NFFT:Fs/2-Fs/NFFT;
+    Ts = 1/Fs;
+    W1 = 15;
+    W2 = 200;
+    F1 = W1/(2*pi);
+    F2 = W2/(2*pi);
+    samples = 500;                  
+    n = 0:samples-1;
 
-    spectrum_original = fftshift(fft(x,NFFT)*(1/Fs));
+    x = sin(2*pi*F1*n*Ts)+0.25*sin(2*pi*F2*n*Ts);
+    F = -Fs/2:Fs/samples:Fs/2-Fs/samples;                    
+    X = fftshift(fft(x));  
+
     %x pass by the hamming filter
     filteredHam1 = filter(H_hamming1,1,x);
     filteredHam2 = filter(H_hamming2,1,x);
     %find the spectrum
-    spectrum_filteredHam1 = fftshift(fft(filteredHam1,NFFT)*(1/Fs));
-    spectrum_filteredHam2 = fftshift(fft(filteredHam2,NFFT)*(1/Fs));
+    spectrum_filteredHam1 = fftshift(fft(filteredHam1));
+    spectrum_filteredHam2 = fftshift(fft(filteredHam2));
     %x pass by the hanning filter
     filteredHan1 = filter(H_hanning1,1,x);
     filteredHan2 = filter(H_hanning2,1,x);
     %find the spectrum
-    spectrum_filteredHan1 = fftshift(fft(filteredHan1,NFFT)*(1/Fs));
-    spectrum_filteredHan2 = fftshift(fft(filteredHan2,NFFT)*(1/Fs));
-
+    spectrum_filteredHan1 = fftshift(fft(filteredHan1));
+    spectrum_filteredHan2 = fftshift(fft(filteredHan2));
 
     %ploting for hamming
     figure;sgtitle('Hamming');
     subplot(3,1,1);
-    plot(F, abs(spectrum_original),'LineWidth',2);
+    plot(F, abs(X),'LineWidth',2);
     grid on;title('Spectrum X');xlabel('Frequency (Hz)');ylabel('|X(f)|');
     subplot(3,1,2);
     plot(F, abs(spectrum_filteredHam1),'LineWidth',2);
@@ -79,7 +84,7 @@ function passSingalFromFiltersWithHz(Fs,H_hamming1,H_hamming2,H_hanning1,H_hanni
     %ploting for hanning
     figure;sgtitle('Hanning');
     subplot(3,1,1);
-    plot(F, abs(spectrum_original),'LineWidth',2);
+    plot(F, abs(X),'LineWidth',2);
     grid on;title('X');xlabel('Frequency (Hz)'); ylabel('|X(f)|');
     subplot(3,1,2);
     plot(F, abs(spectrum_filteredHan1),'LineWidth',2);
